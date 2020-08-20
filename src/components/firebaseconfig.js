251266c27c815700f0b031bff5d17ca7732acf56
Babu Imam <babubpx2762@gmail.com>
2020-08-20 15:22:27 -0700

    configure firebase in the app

---
+++ src/components/firebaseconfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDx-RYiQHbanqHMvxeAH-lR7FbmCqfZyDg",
+    authDomain: "result-verification.firebaseapp.com",
+    databaseURL: "https://result-verification.firebaseio.com",
+    projectId: "result-verification",
+    storageBucket: "result-verification.appspot.com",
+    messagingSenderId: "803525889071",
+    appId: "1:803525889071:web:df594a08da5628909b4ada",
+    measurementId: "G-2P14M0M8ER"
+}
