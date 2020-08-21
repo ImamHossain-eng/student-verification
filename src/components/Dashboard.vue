aac19ff0a902c79ea34ca90ff07a768e42e569a3
Babu Imam <babubpx2762@gmail.com>
2020-08-20 22:39:25 -0700

    show and delete option implemented

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,19 @@
 <template>
     <div id="dashboard">
-        <h2>Dashboard</h2>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Students' List</h4>
+            </li>
+            <li v-for="student in students" v-bind:key="student.id" class="collection-item">
+                <strong>Overview:</strong> {{student.name}}----
+                {{student.student_id}} ----  {{student.position}} of {{student.dept}}
+
+                <router-link class="secondary-content" v-bind:to="{name:'view-student', params: {student_id: student.student_id}}">
+                    <i class="fa fa-eye"></i>
+                </router-link>
+
+            </li>
+        </ul>
         <div class="fixed-action-btn">
             <router-link to="/new" class="btn-floating btn-large red">
               <i class="fa fa-plus"></i>
@@ -10,12 +23,28 @@
 </template>
 
 <script>
+import db from './firebaseinit'
     export default {
         name: 'dashboard',
         data () {
             return {
-
+                students: []
             }
+        },
+        created () {
+            db.collection('students').get().then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    const data = {
+                        'id': doc.id,
+                        'student_id': doc.data().student_id,
+                        'name': doc.data().name,
+                        'dept': doc.data().dept,
+                        'position': doc.data().position
+                        
+                    }
+                    this.students.push(data)
+                })
+            })
         }
     }
 </script>
--- src/components/ViewStudent.vue
+++ src/components/ViewStudent.vue
@@ -1,15 +1,76 @@
 <template>
     <div id="view-student">
-        <h2>View page</h2>
+        <ul class="collection with-header">
+            <li class="collection-header">
+               <strong>
+                    {{name}}
+               </strong>
+            </li>
+            <li class="collection-item">
+                 Student's ID: {{student_id}}
+            </li>
+            <li class="collection-item">
+                 Department: {{dept}} 
+            </li>
+            <li class="collection-item">
+                 Position: {{position}}
+            </li>
+        </ul>
+        <router-link to="/" class="btn grey">Back</router-link>
+        <button @click="deleteStudent" class="btn red">Delete</button>
     </div>
 </template>
 
 <script>
+import db from './firebaseinit'
     export default {
         name: 'view-student',
         data () {
             return {
-
+                student_id: null,
+                name: null,
+                dept: null,
+                position: null
+            }
+        },
+        beforeRouteEnter (to, from, next) {
+            db.collection('students').where('student_id', '==', to.params.student_id).get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    next(vm => {
+                        vm.student_id = doc.data().student_id
+                        vm.name = doc.data().name
+                        vm.dept = doc.data().dept
+                        vm.position = doc.data().position
+                    })
+                })
+            }) 
+        },
+        watch: {
+            '$route': 'fetchData'
+        },
+        methods: {
+            fetchData () {
+                db.collection('students').where('student_id', '==',this.$route.params.student_id).get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        this.student_id = doc.data().student_id
+                        this.name = doc.data().name
+                        this.dept = doc.data().dept
+                        this.position = doc.data().position
+                    })
+                })
+            },
+            deleteStudent () {
+                if(confirm('Are you sure?')) {
+                    db.collection('students').where('student_id', '==',this.$route.params.student_id).get()
+                    .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                       doc.ref.delete()
+                       this.$router.push('/')
+                    })
+                })
+                }
             }
         }
     }
